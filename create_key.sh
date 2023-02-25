93e83738be3b658d8f4ff049fdaa9b1f984ddbfa
Michael Pacheco <git@michaelpacheco.org>
2023-02-25 15:19:25 -0800

    use tabs and add newline.

--- create_key.sh
+++ create_key.sh
@@ -3,11 +3,11 @@ FILE=~/.ssh/id_ed25519.pub
 if [[! -f "$FILE"]]; then
   ssh-keygen -t ed25519
 fi
-echo -e "Host github.com
-	PreferredAuthentications publickey
-	IdentitiesOnly yes
-	IdentityFile ~/.ssh/github_id_ed25519
-	User git" >> ~/.ssh/config
+echo -e "\nHost github.com
+\tPreferredAuthentications publickey
+\tIdentitiesOnly yes
+\tIdentityFile ~/.ssh/id_ed25519
+\tUser git\n" >> ~/.ssh/config
 id=$(cat ~/.ssh/id_ed25519.pub)
 echo "Please paste the following into github\n\n$id"
 
